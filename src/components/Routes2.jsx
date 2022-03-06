f2d6998d01fbf180e3a9cf76cdb9cf64ef9ab71b
Andrew <andrew.yip22@gmail.com>
2022-03-05 18:05:00 -0800

    added routing but recieving multiple errors 🆘

---
+++ src/components/Routes2.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link, Routes, Route, Navigate } from "react-router-dom";
+import { Results } from './Results';
+
+export const Routes2 = () => {
+  return (
+    <div className="p-4">
+        <Routes>
+            <Route exact path="/">
+              
+            </Route>
+            <Route exact path = "/search">
+              
+            </Route>
+            <Route exact path = "/images">
+              
+            </Route>
+            <Route exact path = "/news">
+              
+            </Route>
+            <Route exact path = "/news">
+               
+            </Route>
+        </Routes>
+        
+    </div>
+  )
+}
