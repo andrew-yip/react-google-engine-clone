c24303698df263e5079cd0217f496697cd2d9f17
Andrew <andrew.yip22@gmail.com>
2022-04-07 21:21:36 -0700

    api not fetching images correctly

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -10,8 +10,15 @@ export const Results = () => {
   const location = useLocation(); // images, news, videos, 
 
   useEffect(() => {
-    getResults('/search/q=JavaScript Mastery&num=40')
-  }, []);
+    if (searchTerm){
+      if(location.pathname === 'videos'){
+        getResults(`/search/q=${searchTerm} videos`);
+      } else {
+        getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+      }
+    }
+    
+  }, [searchTerm, location.pathname]);
 
   if (isLoading) return <Loading />
 
@@ -34,7 +41,18 @@ export const Results = () => {
         </div>
       )
     case '/images':
-      return 'IMAGES';
+      return (
+        <div className="flex flex-wrap justify-center items-center">
+          {results?.image_results?.map(({image, link: {href, title}}, index) => (
+            <a className="sm:p-3 p-5" href={href} key={index} target="_blank" rel="noreferrer">
+              <img src={image?.src} alt={title} loading="lazy"/> 
+              <p className="w-36 break-words text-sm mt-2">
+                  {title}
+              </p>
+            </a>
+          ))}
+        </div>
+      )
       case '/news':
         return 'news';
       case '/videos':
