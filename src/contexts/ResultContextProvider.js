6855cb6186ec5bbb103861136a44e2a0e6d6243e
Andrew <andrew.yip22@gmail.com>
2022-05-17 17:32:37 -1000

    added links component

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -6,7 +6,7 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ( {children} ) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('Software Engineering');
+    const [searchTerm, setSearchTerm] = useState('Coding');
 
     // /videos, /search, /images
     // to make api calls
