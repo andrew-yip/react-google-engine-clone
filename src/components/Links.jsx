632a180fbdd8d2dbc38fbdb154f4616fe26e8da2
Andrew <andrew.yip22@gmail.com>
2022-05-18 23:11:05 -1000

    added array of links to links component

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+const links = [
+    { url: '/search', text: '🔎 All' },
+    { url: '/news', text: '📰 News' },
+    { url: '/images', text: '📸 Images' },
+    { url: '/videos', text: '📺 Videos' },
+  ];
 
 export const Links = () => {
   return (
