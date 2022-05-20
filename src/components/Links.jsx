7307ec6fdfe0b64a06ce01bba684ec77a5ec1d33
Andrew <andrew.yip22@gmail.com>
2022-05-19 14:43:49 -1000

    completed links component

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -10,6 +10,10 @@ const links = [
 
 export const Links = () => {
   return (
-    <div>Links</div>
+    <div className='flex sm:justify-around justify-between items-center mt-4'>
+        {links.map(( {url, text}) => (
+            <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
+        ))}
+    </div>
   )
 }
